388d7ceabcd6a1c66b56d90a209e223745694f29
Francisco <50156336@est.ort.edu.ar>
2025-09-25 14:26:05 -0300

    Subo progreso

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,8 +1,30 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
 
-subscribePOSTEvent ("mensaje", (data) => {
-  let leerArchivoFRONT = JSON.parse (fs.readFileSync ("front end/inicio de sesion/index.js"));
-  data.find ()
+subscribePOSTEvent ("register", (data) => {
+  let leer = JSON.parse (fs.readFileSync ("data/registro + login.json", "utf-8"));
+  let objeto = {email: data.email, password: data.password};
+  
+  if (data.email & data.password !== leer) {
+    leer.push (objeto);
+  }
+  else if (data.email & data.password === leer) {
+    data.mensaje = 'Usuario ya está';
+  }
+
+
+  fs.writeFileSync ("data/registro + login.json", JSON.stringify (leer, null, "\n"), {encoding: "utf-8"});
 })
+
+subscribePOSTEvent ("login", (data) => {
+  let leer = JSON.parse (fs.readFileSync ("data/registro + login", "utf-8"));
+  let encontrar = leer.find (leer => data.email === leer.email & data.password === leer.password);
+  if (encontrar) {
+    return {ok: true};
+  }
+  else {
+    return {ok: false};
+  }
+})
+
 startServer ();
