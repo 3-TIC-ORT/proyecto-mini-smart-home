510af087d3f4c28c652c5e0322a61ce7647a9933
Mica <you@example.com>
2025-11-12 10:11:05 -0300

    fran te devuelvo tus cosas

--- back end/INICIO/index.js
+++ back end/INICIO/index.js
@@ -1,5 +1,7 @@
 import fs from "fs";
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
 
 
 subscribePOSTEvent ("register", (data) => {
@@ -12,9 +14,12 @@ subscribePOSTEvent ("register", (data) => {
     return {ok: false};
   }
 
+
   leer.push (objeto);
 
+
   fs.writeFileSync ("data/registro_login.json", JSON.stringify (leer, null, 2), {encoding: "utf-8"});
+
 
   return {ok: true};
 });
@@ -22,9 +27,15 @@ subscribePOSTEvent ("register", (data) => {
 
 
 
+
+
+
+
 subscribePOSTEvent ("login", (data) => {
 
+
   let leer = JSON.parse (fs.readFileSync ("data/registro_login.json", "utf-8"));
+
 
   for (let i = 0; i < leer.length; i++ ) {
    
@@ -33,6 +44,7 @@ subscribePOSTEvent ("login", (data) => {
   if (encontrar) {
     let objeto = {email: data.email, password: data.password};
     leer.push (objeto);
+
 
     fs.writeFileSync ("data/registro_login.json", JSON.stringify (leer, null, 2), {encoding: "utf-8"});
     return {ok: true};
@@ -46,7 +58,15 @@ subscribePOSTEvent ("login", (data) => {
 
 
 
+
+
+
+
 //Para los modos:
+
+
+
+
 
 
 
@@ -58,21 +78,26 @@ subscribePOSTEvent ("crearModo", (data, respuesta) => {
     condiciones: data.condiciones
   }
 
+
   modos.push (objeto);
 
+
   fs.writeFileSync ("data/modos.json", JSON.stringify (modos, null, 2), {encoding: "utf-8"});
-  
+ 
   return (respuesta, {ok: true});
 });
+
 
 subscribeGETEvent ("obtenerModos", () => {
   let modos = fs.readFileSync ("data/modos.json", "utf-8");
   return modos;
 });
 
+
 //Para mostrar el nombre de usuario:
 subscribeGETEvent("obtenerUsuario", () => {
   const leer = JSON.parse(fs.readFileSync("data/registro_login.json", "utf-8"));
+
 
   // Tomar el último usuario con nombre y cumple definidos
   for (let i = leer.length - 1; i >= 0; i--) {
@@ -82,10 +107,216 @@ subscribeGETEvent("obtenerUsuario", () => {
     }
   }
 
+
   return {};
 });
 
-//Para ejecutar el modo: Recibe la info del usuario como forma de objeto y realiza ifs en base a eso.
+
+subscribePOSTEvent ("ejecutarModo", (data) => {
+  let objeto = {
+    persiana: data.persiana,
+    ventilador: data.ventilador,
+    lucesrojas: data.lucesrojas,
+    lucesazules: data.lucesazules
+  };
+
+
+  if (objeto.persiana === 1) {
+    port.write ('d', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'd';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.persiana === 0) {
+    port.write ('a', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err,message);
+      }
+    });
+    let caracter = 'a';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.ventilador === 1) {
+    port.write ('r', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'r';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.ventilador === 0) {
+    port.write ('m', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'm';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.lucesrojas === 1) {
+    port.write ('j', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'j';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.lucesrojas === 0) {
+    port.write ('t', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 't';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.lucesazules === 1) {
+    port.write ('')
+  }
+
+
+});
+
+
+
+
+//Comunicación front-back-hardware: usando Node SerialPort
+
+
+
+
+let port = new SerialPort ({
+  path: 'COM5',
+  baudRate: 9600
+});
+
+
+let parser = port.pipe (new ReadlineParser ({delimiter: "\n"}));
+
+
+//Código que hace que reciba los datos que manda el arduino, y hace que no se muestre un loop infinito de mensajes:
+parser.on('data', (line) => {
+  if (line.trim() !== "Distancia detectada: 0 cm") {
+    console.log('Arduino respondió con:', line);
+  }
+});
+
+
+//Control LEDs rojos (j significa prender, t significa apagar): Le escribo al puerto serie y lo recibe con serial.read
+subscribePOSTEvent("controlLucesLEDr", (data) => {
+  let objeto = { fila: data.fila, intensidad: data.intensidad };
+
+
+  if (objeto.fila === 1 && objeto.intensidad >= 1) {
+    port.write('j', (err) => {
+      if (err) {
+        return console.error('Error al escribir en el puerto ', err.message);
+      }
+      let caracter = 'j';
+      return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+    });
+  } else if (objeto.fila === 1 && objeto.intensidad === 0) {
+    port.write('t', (err) => {
+      if (err) {
+        return console.error('Error al escribir por el puerto: ', err.message);
+      }
+      let caracter = 't';
+      return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+    });
+  }
+});
+
+
+
+
+//Control LEDs azules (o significa prenderlas, sino se apagan):
+subscribePOSTEvent ("controlLucesLEDa", (data) => {
+  let objeto = {fila: data.fila, intensidad: data.intensidad};
+ 
+  if (objeto.intensidad === 1) {
+    port.write ('')
+  }
+});
+
+
+//Control ventilador (r significa prendido, m significa apagado): Le escribo al puerto serie y lo recibe con serial.read
+subscribePOSTEvent ("controlVentilador", (data) => {
+  let objeto = {estado: data.estado};
+
+
+  if (objeto.estado === 1) {
+    port.write ('r', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'r';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.estado === 0) {
+    port.write ('m', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'm';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+});
+
+
+//Control persiana (d significa abajo, a significa arriba): Le escribo al puerto serie y lo recibe con serial.read
+subscribePOSTEvent ("controlPersiana", (data) => {
+  let objeto = {estado: data.estado};
+
+
+  if (objeto.estado === 1) {
+    port.write ('d', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+   });
+   let caracter = 'd';
+   return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+  else if (objeto.estado === 0) {
+    port.write ('a', (err) => {
+      if (err) {
+        return console.error ('Error al escribir por el puerto: ', err.message);
+      }
+    });
+    let caracter = 'a';
+    return (`Caracter escrito exitosamente por el puerto: ${caracter}`);
+  }
+
+
+});
+
+
 
 
 startServer ();
+
